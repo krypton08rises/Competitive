0c1d08784f4c20d8845604b63c1f6729037c10bc
Kushagra Raina <kushagra.raina@gmail.com>
2019-06-05 21:56:20 +0530

    Need help with the Segmentation fault.

--- MergeIntervals/main.c
+++ MergeIntervals/main.c
@@ -14,58 +14,43 @@
  * len : populate the length of returned array of intervals in len
  */
 interval* insert(interval *intervals, int sz, interval newInterval, int *len) {
-    int i,j,k,check=0;
+    int i,check=0;
     interval *A = (interval*)malloc(sizeof(interval)*sz);
     for(i=0;i<sz;++i){
         if(check!=0){
-            if(intervals[i]->start<newInterval.end && intervals[i]->end<newInterval.end){
-                intervals[i]->start = newInterval->start;
-                intervals[i]->end = newInterval->end;
+            if(intervals[i].start<newInterval.end && intervals[i].end<newInterval.end){
+                A[i-1].end = newInterval.end;
                 continue;
             }
-            else if(intervals[i]->start<newInterval->end){
-                intervals[j]->end = intervals[i]->end;
+            else if(intervals[i].start<newInterval.end){
+                A[i-1].end = intervals[i].end;
                 }
         }
-        else if(intervals[i]->start-newInterval->start>0){  //in case we find range start
-            intervals[i]->start = newInterval->start;       //less than intervals start
-            check = 1;                                      //we
-            if(intervals[i]->end<newinterval->end)
-                intervals[i]->end = newinterval->end;
+        else if(intervals[i].start-newInterval.start>0){  //in case we find range start
+            A[i].start = newInterval.start;               //less than intervals start
+            check = 1;
+            if(A[i].end<newinterval.end)
+                A[i].end = newinterval.end;
+            else A[i].end = intervals[i].end;
         }
-        else if(intervals[i]->end-newInterval->start>0){
-            intervals[i]->end = newInterval->end;
+        else if(intervals[i].end-newInterval.start>0){    //in case we find range start
+            A[i].end = newInterval.end;                   // between start and end of an
+            A[i].start = intervals[i].start;              // interval.(only 2 beginning cases)
             check=1;
             j=i;
         }
 
-        else if(intervals[i]->end<newInterval->start && intervals[i+1]->start>newInterval->end){
+        else if(intervals[i].end<newInterval.start && intervals[i+1].start>newInterval.end){
             A = (interval*)realloc(intervals,sizeof(interval)*(sz+1));
             for(k=sz;k>i+1;k--){
-                A[k+1]->start = intervals[k]->start;
-                A[k+1]->end = intervals[k]->end;
+                A[k+1].start = intervals[k].start;
+                A[k+1].end = intervals[k].end;
             }
-            A[i+1]->start = newInterval->start;
-            A[i+1]->end = newInterval->end;
-            return A;
+            A[i+1].start = newInterval.start;
+            A[i+1].end = newInterval.end;
         }
-        A[i]->start = intervals[i]->start;      // in case nos are not smaller than
-        A[i]->end = intervals[i]->end;          // new interval
+        A[i].start = intervals[i].start;      // in case nos are not smaller than
+        A[i].end = intervals[i].end;          // new interval
     }
+    return A;
 }
-        /*
-
-    for(i=0;i<sz;++i){
-        if(intervals[i].start==newInterval.start && intervals[i].end==newInterval.end)
-            continue;
-        if(intervals[i].end==newInterval.start && intervals[i+1].start==newInterval.end){
-            intervals[i].end = intervals[i+1].end;
-            for(j=i+1;j<sz-1;++j){
-                intervals[j].start = intervals[j+1].start;
-                intervals[j].end = intervals[j+1].end;
-            }
-        }
-    }
-    return intervals;
-}
-*/
