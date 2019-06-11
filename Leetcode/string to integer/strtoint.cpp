74d5efd17bdc0c4a4993fdfbf29e4143dde2ae16
Kushagra Raina <kushagra.raina@gmail.com>
2019-06-11 22:04:50 +0530

    changed atoi to stoi

--- Leetcode/string to integer/strtoint.cpp
+++ Leetcode/string to integer/strtoint.cpp
@@ -13,6 +13,29 @@ public:
         k++;
         j++;
     }
+        if(stoi(buffer)==0 && int(buffer[j])==45)
+            return INT_MIN;
+        else if(stoi(buffer)==0)
+            return INT_MAX;
+        long long int i = stoi(buffer);
+        return i;
+    }
+};
+/*class Solution {
+public:
+    int myAtoi(string str) {
+        string buffer;
+        int j=0,k=0;
+        while(( int(str[j])<49 || int(str[j])>58 ) && int(str[j]!=45)){
+        j++;
+        }
+        if(j==str.size())
+            return 0;
+        while(j<str.size()){
+        buffer[k] = str[j];
+        k++;
+        j++;
+    }
         if(atoi(buffer.c_str())==0 && int(buffer[j])==45)
             return INT_MIN;
         else if(atoi(buffer.c_str())==0)
@@ -21,3 +44,4 @@ public:
         return i;
     }
 };
+*/
