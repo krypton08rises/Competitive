7ae7b72899edb33e1f9c1bafaee55cd3a3bd41a5
Kushagra Raina <kushagra.raina@gmail.com>
2019-06-24 11:48:52 +0530

    runtime error

--- Codechef/main.cpp
+++ Codechef/main.cpp
@@ -5,21 +5,40 @@ using namespace std;
 
 int main()
 {
-    int n,t,c;
+    int n,t,c,i,j;
+
     cin>>t;
-    string str;
-      for(int i=0;i<t;i++){
-        cin>>n>>c;
-        vector <int> v;
-        getline(cin,str);
-        for(int i=0,j=0;i<str.size();i++){
-            if(str[i]!=' '){
-                v[j]=str[i];
-                j++;
+
+    int num;
+    vector <int> v;
+    vector <string> str;
+
+    for(i=0;i<t;i++){
+
+            int sum=0;
+            cin>>n>>c;
+
+            for(j=0;j<n;j++){
+                        cin>>num;
+                        v.push_back(num);
+                    }
+
+            for(j=0;j<v.size();j++){
+                        sum+=v[j];
             }
+
+            if(sum<=c)
+
+                    str[i]="yes";
+
+            else
+
+                    str[i]="no";
+            v.clear();
         }
-        for(int i=0;i<vector.size();i++){
-            cout<<v[i];
-        }
+
+        for(i=0;i<str.size();i++)
+                cout<<str[i]<<endl;
+
     }
-}
+
