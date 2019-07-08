217edfe7a1d9f2d9ac4c0804f956632597a58c62
Kushagra Raina <kushagra.raina@gmail.com>
2019-07-08 16:17:14 +0530

    need to avoid duplicates

--- Codechef/July challenge/parityagain/main.cpp
+++ Codechef/July challenge/parityagain/main.cpp
@@ -7,65 +7,63 @@ using namespace std;
 int isevenones(int n);
 
 int main(){
-        int t,i,j,q,k,temp,evensum,oddsum,l=0;
-        map<int, int> map1;
+        int t,i,j,q,k,l=0,temp,evensum,oddsum;
+//        map<int, int> map1;
         vector<int> v,res;
         cin>>t;
-        vector<int>::iterator it=v.begin();
 
         for(i=0;i<t;i++){
 
                 cin>>q;
                 evensum=0;
                 oddsum=0;
-                for(j=0;j<q;j++,it++){
 
-                        v.push_back(0);
-                        res.push_back(0);
-                        cin>>v[j];
-
-                        if(map1[v[j]]!=v[j])
-                                map1.insert(pair<int, int>(v[j],v[j]));
-
-                        else v.erase(it);
+                for(j=0;j<q;j++){
+                            v.push_back(0);
+                            cin>>v[j];
                     }
+                    vector<int>::iterator ip;
+                            ip = std::unique(v.begin(), v.begin() + j);
+                            v.resize(std::distance(v.begin(), ip));
 
-                     for(k=0,j=0;k<j;k++,l+=2,j++){
-                                temp = v[k]^v[j];
 
-                                if(map1[temp]==temp){
-                                        map1.insert(pair<int,int>(temp,temp));
-                                        if(isevenones(v[j]))
+                for(j=0;j<q;j++){
+                                res.push_back(0);
+                                res[l] = v[j];
+                                l++;
+                                if(isevenones(v[j]))
                                                 evensum++;
                                             else
                                                     oddsum++;
-                                        if(isevenones(temp))
-                                                evensum++;
-                                            else
-                                                    oddsum++;
-                                    }
+                                for(k=0;k<j;k++){
+                                            res.push_back(0);
+                                            res[l] = res[k]^v[j];
+                                            if(isevenones(res[l])==0)
+                                                        evensum++;
+                                                    else
+                                                            oddsum++;
+                                                l++;
+                                }
                                     cout<<evensum<<" "<<oddsum<<"\n";
-                            }
 
+                        }
             }
 }
 
 
-int evenones(int num){
+int isevenones(int num){
     int count=1;
 
         if(num==0)
             return 1;
 
         if(ceil(log2(num)) == floor(log2(num)))
-                return log2(num);
+                return 0;
             else {
                 while(num!=0){
                         num-=pow(2,floor(log2(num)));
                         count++;
                     }
             }
-                if(count%2==0)
-                    return 1;
-                return 0;
+                return (count%2==0)? 1 : 0 ;
         }
