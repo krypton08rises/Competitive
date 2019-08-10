b83837a21f2bc4abdbb09b1e07936eec00986510
Kushagra Raina <kushagra.raina@gmail.com>
2019-08-11 02:10:04 +0530

    error when number goes out of bound and becomes 0

--- Codechef/August long challenge/distapples/main.cpp
+++ Codechef/August long challenge/distapples/main.cpp
@@ -2,23 +2,22 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
-    int t,i,n,j,k;
+int main() {
+    int t,i,j;
+    long long int n,k,temp;
     cin>>t;
     vector<string> res;
     for(i=0;i<t;i++){
             cin>>n>>k;
             res.push_back("YES");
-            if(n%k!=0)
-                return 0;
-            if(k==1 || n==0){
+            if(k==1){
                 res[i] = "NO";
                 continue;
             }
             if(n==k)
                 continue;
-            if(n%(k*k)==0 || n)
+            temp = n/k;
+            if(temp%k==0)
                 res[i]="NO";
     }
     for(i=0;i<t;i++)
--- Leetcode/chardiff/main.cpp
+++ Leetcode/chardiff/main.cpp
@@ -1,21 +1,28 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int i=0,j=0,count=0,index;
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s = "pwwkew";
+   int i=0,j=0,count=0,index;
         array<int,26> arr{0},temp{0};
         while(i<s.size()){
             index = s[i]-'a';
-            if(temp[index]>1){
-                    copy(arr.begin(), arr.begin()+26, temp.begin());
+            if(temp[index]>0){
+                    copy(arr.begin(), arr.end(), temp.begin());
                     j++;
                     i=j;
                     continue;
             }
-                if(j-i>count)
-                        count = j-i;
                 temp[index]++;
                 i++;
+
+                if(i-j>count)
+                        count = i-j;
         }
-        return count;
+        cout<<count;
     }
-};
+
