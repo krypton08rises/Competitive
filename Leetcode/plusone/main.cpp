4e6009e280f38a8ee63149ccc62a54d626e424a4
Kushagra Raina <kushagra.raina@gmail.com>
2019-08-27 23:52:36 +0530

    plus one

---
+++ Leetcode/plusone/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    1/*
+    int n = digits.size();
+	for (int i = n - 1; i >= 0; --i)
+	{
+		if (digits[i] == 9)
+		{
+			digits[i] = 0;
+		}
+		else
+		{
+			digits[i]++;
+			return;
+		}
+	}
+		digits[0] =1;
+		digits.push_back(0);
+		*/
+}
