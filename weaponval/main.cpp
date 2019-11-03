d92d7a28120bfc26cc6d7531ceb708fd5c1cd071
Kushagra Raina <kushagra.raina@gmail.com>
2019-11-03 19:28:25 +0530

    november long challenge

---
+++ weaponval/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int  t,n,i,j;
+    cin>>t;
+    vector<string> str;
+    for(i=0;i<t;i++)
+    {
+        cin>>n;
+        for(j=0;j<n;j++)
+        {
+                str.push_back("");
+                cin>>str[j];
+        }
+
+    }
+}
